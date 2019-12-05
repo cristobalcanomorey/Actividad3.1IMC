5b4a1b333373e0932b9321f366a197a721501d05
Tofol <tofolcanodaw2@gmail.com>
2019-12-05 18:48:15 +0100

    scrbien

--- src/script.sql
+++ src/script.sql
@@ -7,7 +7,8 @@ CREATE USER IF NOT EXISTS 'tofol'@'localhost' IDENTIFIED BY '1234';
 GRANT ALL PRIVILEGES ON ActividadIMC.* TO 'tofol'@'localhost' WITH GRANT OPTION;
 
 CREATE TABLE IF NOT EXISTS usuario (
-	correo VARCHAR(100) NOT NULL PRIMARY KEY,
+	id INT(11) auto_increment PRIMARY KEY,
+	correo VARCHAR(100) NOT NULL,
 	nombre VARCHAR(100) NOT NULL,
 	password VARCHAR(100) NOT NULL,
 	foto VARCHAR(255),
@@ -15,12 +16,11 @@ CREATE TABLE IF NOT EXISTS usuario (
     fechaRegistro DATE NOT NULL
 )  ENGINE=INNODB charset=utf8;
 
-/*Foreign keys no funcionan*/
 CREATE TABLE IF NOT EXISTS validacion (
 	codigo VARCHAR(100) NOT NULL PRIMARY KEY,
-    idUsuario VARCHAR(100) NOT NULL,
+    idUsuario INT(11) NOT NULL,
     foreign key (idUsuario)
-   	 references usuario (correo)
+   	 references usuario (id)
     	on delete cascade
     	on update cascade
 )  ENGINE=INNODB;
@@ -29,9 +29,9 @@ CREATE TABLE IF NOT EXISTS calculo (
 	id INT(11) AUTO_INCREMENT PRIMARY KEY,
     estatura float NOT NULL,
     peso float NOT NULL,
-    idUsuario VARCHAR(100) NOT NULL,
+    idUsuario INT(11) NOT NULL,
     foreign key (idUsuario)
-   	 references usuario (correo)
+   	 references usuario (id)
     	on delete cascade
     	on update cascade
 )  ENGINE=INNODB;
