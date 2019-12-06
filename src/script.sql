2b1b1799bbd192ca6146fdf39a0a2a7ebf871eeb
tofol <tofolcanodaw2@gmail.com>
2019-12-06 18:26:03 +0100

    scriptin

--- src/script.sql
+++ src/script.sql
@@ -2,9 +2,14 @@ CREATE DATABASE IF NOT EXISTS ActividadIMC;
 
 use ActividadIMC;
 
-CREATE USER IF NOT EXISTS 'tofol'@'localhost' IDENTIFIED BY '1234';
+SET GLOBAL validate_password_policy = 0;
 
-GRANT ALL PRIVILEGES ON ActividadIMC.* TO 'tofol'@'localhost' WITH GRANT OPTION;
+DROP USER IF EXISTS 'tofol'@'localhost';
+DROP USER IF EXISTS 'tofol'@'%';
+
+CREATE USER IF NOT EXISTS 'tofol'@'%' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON ActividadIMC.* TO 'tofol'@'%' WITH GRANT OPTION;
 
 CREATE TABLE IF NOT EXISTS usuario (
 	id INT(11) auto_increment PRIMARY KEY,
@@ -13,7 +18,7 @@ CREATE TABLE IF NOT EXISTS usuario (
 	password VARCHAR(100) NOT NULL,
 	foto VARCHAR(255),
     validado BOOLEAN NOT NULL,
-    fechaRegistro DATE NOT NULL
+    fechaRegistro DATETIME NOT NULL
 )  ENGINE=INNODB charset=utf8;
 
 CREATE TABLE IF NOT EXISTS validacion (
@@ -29,10 +34,55 @@ CREATE TABLE IF NOT EXISTS calculo (
 	id INT(11) AUTO_INCREMENT PRIMARY KEY,
     estatura float NOT NULL,
     peso float NOT NULL,
-    fecha DATE NOT NULL,
+    fecha DATETIME NOT NULL,
     idUsuario INT(11) NOT NULL,
     foreign key (idUsuario)
    	 references usuario (id)
     	on delete cascade
     	on update cascade
 )  ENGINE=INNODB;
+
+INSERT INTO usuario (correo,nombre,password,foto,validado,fechaRegistro)
+values ('tofol@gmail.com','tofol','passwordtofol','rutafototofol',false,'2019-12-06 17:20:00');
+
+INSERT INTO usuario (correo,nombre,password,foto,validado,fechaRegistro)
+values ('pep@gmail.com','pep','passwordpep','rutafotopep',true,'2019-12-07 17:20:00');
+
+INSERT INTO usuario (correo,nombre,password,foto,validado,fechaRegistro)
+values ('pip@gmail.com','pip','passwordpip','rutafotopip',true,'2019-12-08 17:20:00');
+
+INSERT INTO usuario (correo,nombre,password,foto,validado,fechaRegistro)
+values ('pop@gmail.com','pop','passwordpop','rutafotopop',false,'2019-12-09 17:20:00');
+
+INSERT INTO usuario (correo,nombre,password,foto,validado,fechaRegistro)
+values ('pup@gmail.com','pup','passwordpup','rutafotopup',true,'2019-12-10 17:20:00');
+
+INSERT INTO validacion (codigo,idUsuario)
+values ('algkkjadkfajfajsdnrriusfksd',1);
+
+INSERT INTO validacion (codigo,idUsuario)
+values ('pydfhrybcnweuqysaixjhfdsuhy',4);
+
+INSERT INTO calculo (estatura,peso,fecha,idUsuario)
+values (1.90,80.7,'2019-12-06 18:14:00',1);
+
+INSERT INTO calculo (estatura,peso,fecha,idUsuario)
+values (1.90,81,'2019-12-07 18:16:00',1);
+
+INSERT INTO calculo (estatura,peso,fecha,idUsuario)
+values (1.90,86,'2019-12-10 20:00:00',1);
+
+INSERT INTO calculo (estatura,peso,fecha,idUsuario)
+values (1.90,100.4,'2020-01-01 00:00:14',1);
+
+INSERT INTO calculo (estatura,peso,fecha,idUsuario)
+values (1.90,101,'2019-12-06 18:14:00',2);
+
+INSERT INTO calculo (estatura,peso,fecha,idUsuario)
+values (1.90,100,'2019-12-06 20:00:00',2);
+
+INSERT INTO calculo (estatura,peso,fecha,idUsuario)
+values (1.90,98,'2019-12-07 04:30:00',2);
+
+INSERT INTO calculo (estatura,peso,fecha,idUsuario)
+values (1.79,68,'2020-01-01 00:00:07',2);
